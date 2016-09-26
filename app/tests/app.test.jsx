64cc0c3e4da512e12b8661381fa86dbe47778782
Deepinder Badesha <dbadesha@salesforce.com>
2016-09-26 16:03:18 -0700

    fix chrome issue

--- app/tests/app.test.jsx
+++ app/tests/app.test.jsx
@@ -1,5 +1,7 @@
 var expect = require('expect');
 
-it('should run this first test', ()=>{
-  expect(1).toBe(1);
+describe('App Tests', ()=>{
+  it('should run this first test', ()=>{
+    expect(1).toBe(1);
+  });  
 });

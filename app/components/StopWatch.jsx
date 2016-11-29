f9ee0f7740c1fa737eb592032caccd764a6fbeae
Deepinder Badesha <dbadesha@salesforce.com>
2016-11-28 17:18:53 -0800

    hook up controls

--- app/components/StopWatch.jsx
+++ app/components/StopWatch.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var Clock = require('Clock');
 var StopWatchForm = require('StopWatchForm');
+var Controls = require('Controls');
 
 var StopWatch = React.createClass({
 	getInitialState: function() {
@@ -13,9 +14,15 @@ var StopWatch = React.createClass({
 	componentDidUpdate: function(prevProps, prevState){
 		if (this.state.countdownStatus !== prevState.countdownStatus) {
 			switch(this.state.countdownStatus){
-				case'started':
-				this.startTimer();
-				break;
+				case 'started':
+					this.startTimer();
+					break;
+				case 'stopped':
+					this.setState({count:0});
+				case 'paused':
+					clearInterval(this.timer)
+					this.timer = undefined;
+					break;
 			}
 		}
 	},
@@ -32,16 +39,28 @@ var StopWatch = React.createClass({
 	handleCountDown: function(seconds){
 		this.setState({
 			count: seconds,
-			countdownStatus: "started"
+			countdownStatus: 'started'
 		});
 	},
 
+	handleStatusChange: function(newStatus){
+		this.setState({countdownStatus:newStatus});
+	},
+
 	render: function(){
-		var {count} = this.state;
+		var {count, countdownStatus} = this.state;
+		var renderControls = ()=>{
+			if (countdownStatus !== 'stopped'){
+				return <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>;
+			} else {
+				return <StopWatchForm onSetCountDown={this.handleCountDown}/>;
+			}
+		};
+
 		return (
 		    <div>
 		    	<Clock millisecs={count}/>
-		    	<StopWatchForm onSetCountDown={this.handleCountDown}/>
+		    	{renderControls()}
 		    </div>
 		);
 	}   
